b19f7a940d76c0b09e7f5fcdc77cdac8b89d876a
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-11 08:33:42 -0500

    feat: :sparkles: add BodegaResponseDTO

--- src/main/java/com/s1/gestion_bodegas/dto/response/BodegaResponseDTO.java
+++ src/main/java/com/s1/gestion_bodegas/dto/response/BodegaResponseDTO.java
@@ -1,4 +1,19 @@
 package com.s1.gestion_bodegas.dto.response;
 
-public record BodegaResponseDTO() {
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public record BodegaResponseDTO(
+        Long id,
+
+        String nombre,
+
+        String ubicacion,
+
+        Integer capacidad,
+
+        String encargado
+) {
 }
