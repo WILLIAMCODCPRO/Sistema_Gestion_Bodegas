406042a9939f50ac8683c312424c7a1b4733a5ff
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-11 17:11:12 -0500

    feat: :sparkles: add InventarioController

---
+++ src/main/java/com/s1/gestion_bodegas/controller/InventarioController.java
@@ -0,0 +1,27 @@
+package com.s1.gestion_bodegas.controller;
+
+import com.s1.gestion_bodegas.dto.response.BodegaResponseDTO;
+import com.s1.gestion_bodegas.dto.response.InventarioResponseDTO;
+import com.s1.gestion_bodegas.service.InventarioService;
+import com.s1.gestion_bodegas.service.impl.InventarioServicelmpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventario")
+@RequiredArgsConstructor
+@Validated
+public class InventarioController {
+    private final InventarioServicelmpl inventarioServicelmpl;
+
+    @GetMapping
+    public ResponseEntity<List<InventarioResponseDTO>> listarBodegas(){
+        return ResponseEntity.ok().body(inventarioServicelmpl.listarInventario());
+    }
+}
