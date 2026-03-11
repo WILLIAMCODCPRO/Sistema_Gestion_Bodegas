11a9ffb76190f3fb461f360521cc75d3b0745852
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-11 07:50:34 -0500

    feat: :sparkles: add model Inventario

---
+++ src/main/java/com/s1/gestion_bodegas/Inventario.java
@@ -0,0 +1,32 @@
+package com.s1.gestion_bodegas;
+
+import com.s1.gestion_bodegas.model.Bodega;
+import com.s1.gestion_bodegas.model.Producto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "detalle_movimiento")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_bodega", nullable = false)
+    private Bodega bodega;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_producto", nullable = false)
+    private Producto producto;
+
+    @Column(nullable = false)
+    private Integer stock_producto;
+}
