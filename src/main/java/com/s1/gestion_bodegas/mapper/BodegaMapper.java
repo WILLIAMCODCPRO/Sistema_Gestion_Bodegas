9176242567b27f5cd710b176557c8efa7f5322de
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-11 10:43:47 -0500

    feat: :sparkles: add mapper.BodegaMapper.endidadADTO and mapper.BodegaMapper.DTOAEntidad

---
+++ src/main/java/com/s1/gestion_bodegas/mapper/BodegaMapper.java
@@ -0,0 +1,33 @@
+package com.s1.gestion_bodegas.mapper;
+
+import com.s1.gestion_bodegas.dto.request.BodegaRequestDTO;
+import com.s1.gestion_bodegas.dto.response.BodegaResponseDTO;
+import com.s1.gestion_bodegas.model.Bodega;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BodegaMapper {
+
+    public BodegaResponseDTO entidadADTO(Bodega bodega) {
+        if (bodega == null) return null;
+        return new BodegaResponseDTO(
+                bodega.getId(),
+                bodega.getNombre(),
+                bodega.getUbicacion(),
+                bodega.getCapacidad(),
+                bodega.getEncargado()
+        );
+    }
+
+    public Bodega DTOAEntidad(BodegaRequestDTO bodegaRequestDTO) {
+        if (bodegaRequestDTO == null) return null;
+        Bodega bodega = new Bodega();
+        bodega.setNombre(bodegaRequestDTO.nombre());
+        bodega.setUbicacion(bodegaRequestDTO.ubicacion());
+        bodega.setCapacidad(bodegaRequestDTO.capacidad());
+        bodega.setEncargado(bodegaRequestDTO.encargado());
+        return bodega;
+
+    }
+
+}
