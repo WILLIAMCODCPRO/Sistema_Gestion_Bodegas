84f1f36d1f20d40b4f841016e44e38b42e920470
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-10 22:07:04 -0500

    feat: 🗄️ add initial database schema

---
+++ database/schema.sql
@@ -0,0 +1,68 @@
+create database LogiTrack;
+
+use LogiTrack;
+
+CREATE TABLE producto (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    categoria VARCHAR(50),
+    precio DECIMAL NOT NULL
+);
+
+CREATE TABLE bodega (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    ubicacion VARCHAR(100) NOT NULL,
+    capacidad INT NOT NULL,
+    encargado VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    rol ENUM('ADMIN','EMPLEADO') NOT NULL
+);
+
+CREATE TABLE movimiento_inventario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fecha DATETIME NOT NULL,
+    id_usuario INT NOT NULL,
+    tipo_movimiento ENUM('ENTRADA','SALIDA','TRASNFERENCIA') NOT NULL,
+    id_bodega_origen INT NOT NULL,
+    id_bodega_destino INT NOT NULL,
+
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+    FOREIGN KEY (id_bodega_origen) REFERENCES bodega(id),
+    FOREIGN KEY (id_bodega_destino) REFERENCES bodega(id)
+);
+
+CREATE TABLE detalle_movimiento (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT NOT NULL,
+    cantidad_producto INT NOT NULL,
+    id_movimiento_inventario INT NOT NULL,
+
+    FOREIGN KEY (id_producto) REFERENCES producto(id),
+    FOREIGN KEY (id_movimiento_inventario) REFERENCES movimiento_inventario(id)
+);
+
+CREATE TABLE inventario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_bodega int NOT NULL,
+    id_producto INT NOT NULL,
+    stock_producto INT NOT NULL,
+
+    FOREIGN KEY (id_bodega) REFERENCES bodega(id),
+    FOREIGN KEY (id_producto) REFERENCES producto(id)
+);
+
+CREATE TABLE auditoria (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_operacion ENUM('INSERT','UPDATE','DELETE') NOT NULL,
+    fecha DATETIME NOT NULL,
+    usuario VARCHAR(50) NOT NULL,
+    entidad VARCHAR(50) NOT NULL,
+    id_fila_modificada INT NOT NULL,
+    columna_modificada VARCHAR(70) NOT NULL,
+    valor_antiguo VARCHAR(200) NOT NULL,
+    valor_nuevo VARCHAR(200) NOT NULL
+);
