ab1e6390b1972c0268fcb9546b2d79f54d6946cd
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-11 06:41:28 -0500

    feat::sparkles:  add model Poducto

--- src/main/java/com/s1/gestion_bodegas/model/Bodega.java
+++ src/main/java/com/s1/gestion_bodegas/model/Bodega.java
@@ -1,4 +1,33 @@
 package com.s1.gestion_bodegas.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "producto")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bodega {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private  String ubicacion;
+
+    @Column(nullable = false)
+    private Integer capacidad;
+
+    @Column(nullable = false)
+    private  String encargado;
 }
