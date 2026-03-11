cf4ae0ae2bb8f15b3cf3c8df2a04bf6d8d2cb0d2
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-11 15:09:29 -0500

    feat: :sparkles: add dto.response.ProductoResponseDTO

---
+++ src/main/java/com/s1/gestion_bodegas/dto/response/ProductoResponseDTO.java
@@ -0,0 +1,14 @@
+package com.s1.gestion_bodegas.dto.response;
+
+import java.math.BigDecimal;
+
+public record ProductoResponseDTO(
+        Long id,
+
+        String nombre,
+
+        String categoria,
+
+        BigDecimal precio
+) {
+}
