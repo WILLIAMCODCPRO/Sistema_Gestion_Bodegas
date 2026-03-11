746589b08d1f4f2ed18834d777ca5ec01931083b
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-11 16:58:37 -0500

    feat: :sparkles: add InventarioServicelmpl

--- src/main/java/com/s1/gestion_bodegas/repository/ProductoRepository.java
+++ src/main/java/com/s1/gestion_bodegas/repository/ProductoRepository.java
@@ -1,10 +1,10 @@
 package com.s1.gestion_bodegas.repository;
 
-import com.s1.gestion_bodegas.model.Bodega;
 import com.s1.gestion_bodegas.model.Inventario;
+import com.s1.gestion_bodegas.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InventarioRepository extends JpaRepository<Inventario,Long> {
+public interface ProductoRepository extends JpaRepository<Producto,Long> {
 }
