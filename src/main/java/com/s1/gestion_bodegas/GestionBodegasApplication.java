a5a60485f6b753e26f69a8c9f6ee9f6472c063b0
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-10 20:04:00 -0500

    :sparkles: start project:

---
+++ src/main/java/com/s1/gestion_bodegas/GestionBodegasApplication.java
@@ -0,0 +1,13 @@
+package com.s1.gestion_bodegas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBodegasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBodegasApplication.class, args);
+	}
+
+}
