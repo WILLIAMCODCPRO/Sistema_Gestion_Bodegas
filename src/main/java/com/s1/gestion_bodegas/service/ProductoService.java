a57f85c7f57afda49f55f3cbb609256d13eba946
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-12 10:02:08 -0500

    feat: :sparkles: add ProductoService

---
+++ src/main/java/com/s1/gestion_bodegas/service/ProductoService.java
@@ -0,0 +1,18 @@
+package com.s1.gestion_bodegas.service;
+
+import com.s1.gestion_bodegas.dto.request.ProductoRequestDTO;
+import com.s1.gestion_bodegas.dto.response.ProductoResponseDTO;
+
+import java.util.List;
+
+public interface ProductoService {
+    List<ProductoResponseDTO> listarProductos();
+
+    ProductoResponseDTO registrarProducto(ProductoRequestDTO productoRequestDTO);
+
+    ProductoResponseDTO buscarProductoID(Long id);
+
+    ProductoResponseDTO actualizarProducto(ProductoRequestDTO productoRequestDTO, Long id);
+
+    void eliminarProducto(Long id);
+}
---
+++ src/main/java/com/s1/gestion_bodegas/service/impl/ProductoServiceimpl.java
@@ -0,0 +1,59 @@
+package com.s1.gestion_bodegas.service.impl;
+
+import com.s1.gestion_bodegas.dto.request.ProductoRequestDTO;
+import com.s1.gestion_bodegas.dto.response.ProductoResponseDTO;
+import com.s1.gestion_bodegas.mapper.BodegaMapper;
+import com.s1.gestion_bodegas.mapper.ProductoMapper;
+import com.s1.gestion_bodegas.repository.BodegaRepository;
+import com.s1.gestion_bodegas.repository.ProductoRepository;
+import com.s1.gestion_bodegas.service.ProductoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductoServiceimpl implements ProductoService {
+
+    private final ProductoRepository productoRepository;
+    private final ProductoMapper productoMapper;
+    private final BodegaRepository bodegaRepository;
+    private final BodegaMapper bodegaMapper;
+
+    @Override
+    public List<ProductoResponseDTO> listarProductos() {
+        return productoRepository
+                .findAll()
+                .stream()
+                .map(dato -> productoMapper.entidadADTO(
+                        dato,
+                        bodegaMapper.entidadADTO(
+                                bodegaRepository
+                                        .findById(dato.getBodega().getId())
+                                        .orElseThrow()
+                        )
+                ))
+                .toList();
+    }
+
+    @Override
+    public ProductoResponseDTO registrarProducto(ProductoRequestDTO productoRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public ProductoResponseDTO buscarProductoID(Long id) {
+        return null;
+    }
+
+    @Override
+    public ProductoResponseDTO actualizarProducto(ProductoRequestDTO productoRequestDTO, Long id) {
+        return null;
+    }
+
+    @Override
+    public void eliminarProducto(Long id) {
+
+    }
+}
