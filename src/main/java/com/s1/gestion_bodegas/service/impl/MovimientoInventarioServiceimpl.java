c5d61c271dc784a9a85094ee7716c2c65b8e8b5f
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-13 07:11:29 -0500

    feat: :sparkles: add MovimientoService

---
+++ src/main/java/com/s1/gestion_bodegas/service/impl/MovimientoInventarioServiceimpl.java
@@ -0,0 +1,6 @@
+package com.s1.gestion_bodegas.service.impl;
+
+import com.s1.gestion_bodegas.service.MovimientoInventarioService;
+
+public class MovimientoInventarioServiceimpl implements MovimientoInventarioService {
+}
