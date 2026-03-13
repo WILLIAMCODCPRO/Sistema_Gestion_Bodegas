25f9eaa699f4e042f492b16e63bc5a66533f728e
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-13 07:04:37 -0500

    feat: :sparkles: add MovimientoRequestDTO

--- src/main/java/com/s1/gestion_bodegas/dto/request/MovimientoInventarioRequestDTO.java
+++ src/main/java/com/s1/gestion_bodegas/dto/request/MovimientoInventarioRequestDTO.java
@@ -1,4 +1,18 @@
 package com.s1.gestion_bodegas.dto.request;
 
-public record MovimientoInventarioRequestDTO() {
+import com.s1.gestion_bodegas.model.Bodega;
+import com.s1.gestion_bodegas.model.Producto;
+import com.s1.gestion_bodegas.model.TipoMovimiento;
+
+public record MovimientoInventarioRequestDTO(
+        TipoMovimiento tipoMovimiento,
+
+        Long productoId,
+
+        Integer cantidadProducto,
+
+        Long bodegaOrigenId,
+
+        Long bodegaDestinoId
+) {
 }
