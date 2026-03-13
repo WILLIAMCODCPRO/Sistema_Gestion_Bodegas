df574848976285ac792430e7611de252f3ece56f
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-13 07:02:17 -0500

    feat: :sparkles: add MovimientoResponseDTO

--- src/main/java/com/s1/gestion_bodegas/dto/response/MovimientoInventarioResponseDTO.java
+++ src/main/java/com/s1/gestion_bodegas/dto/response/MovimientoInventarioResponseDTO.java
@@ -1,4 +1,31 @@
 package com.s1.gestion_bodegas.dto.response;
 
-public record MovimientoInventarioResponseDTO() {
+import com.s1.gestion_bodegas.model.Bodega;
+import com.s1.gestion_bodegas.model.Producto;
+import com.s1.gestion_bodegas.model.TipoMovimiento;
+import com.s1.gestion_bodegas.model.Usuario;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
+
+public record MovimientoInventarioResponseDTO(
+       Long id,
+
+       LocalDateTime fecha,
+
+       Usuario usuario,
+
+       TipoMovimiento tipoMovimiento,
+
+       Producto producto,
+
+       Integer cantidadProducto,
+
+       Bodega bodegaOrigen,
+
+       Bodega bodegaDestino
+) {
 }
