e383f2e40db6550d13411ca2a044da457735942d
WILLIAMCODCPRO <williamcodcpro@gmail.com>
2026-03-16 19:18:11 -0500

    feat: :sparkles: update fronted pro

--- src/main/java/com/s1/gestion_bodegas/GestionBodegasApplication.java
+++ src/main/java/com/s1/gestion_bodegas/GestionBodegasApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestionBodegasApplication {
 
 	public static void main(String[] args) {
+        //Bug
 		SpringApplication.run(GestionBodegasApplication.class, args);
 	}
 
